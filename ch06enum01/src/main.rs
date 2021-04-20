f0ddc539b19f3ef8ffdc522929dbbf4f695299f6
liusen <liusen@miliantech.com>
2021-04-20 17:44:38 +0800

    fix

---
+++ ch06enum01/src/main.rs
@@ -0,0 +1,34 @@
+
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+enum IpAddrStr {
+    V4(String),
+    V6(String),
+}
+
+enum IpAddrArrs {
+    V4(u8,u8,u8,u8),
+    V6(String),
+}
+fn main() {
+    println!("Hello, world!");
+    let home = IpAddr{
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1")
+    };
+    let ihome = IpAddrStr::V4(String::from("127.0.0.1"));
+    let iloopback = IpAddrStr::V6(String::from("::1"));
+    
+}
+
+fn route(ip_type: IpAddrKind){
+
+}
