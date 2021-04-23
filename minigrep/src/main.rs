38beb9993a56ea0838dbb00c7f7cbacb54d5e622
liusen <liusen@miliantech.com>
2021-04-23 18:10:44 +0800

    fix

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,20 @@
 use std::env;
+use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}",args);
+    let (queryStr,fileName) = parse_config(&args);
+    /* 
+    let queryStr = &args[1];
+    let fileName = &args[2];
+    */
+    let contents = fs::read_to_string(fileName).expect("something went wrong reading the file");
+    println!("with text: \n{}",contents);    
     println!("Hello, world!");
+}
+
+fn parse_config(args: &[String]) -> (&str,&str){
+    let query = &args[1];
+    let fileName = &args[2];
+    (query,fileName)
 }
