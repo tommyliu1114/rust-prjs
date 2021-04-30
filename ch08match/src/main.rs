0b254b35b7465a34c1642c3e3b24433bcf8428e9
liusen <liusen@miliantech.com>
2021-04-30 17:21:27 +0800

    fix

---
+++ ch08match/src/main.rs
@@ -0,0 +1,53 @@
+enum Color {
+    // 这三个取值仅由它们的名字（而非类型）来指定。
+    Red,
+    Blue,
+    Green,
+    // 这些则把 `u32` 元组赋予不同的名字，以色彩模型命名。
+    RGB(u32, u32, u32),
+    HSV(u32, u32, u32),
+    HSL(u32, u32, u32),
+    CMY(u32, u32, u32),
+    CMYK(u32, u32, u32, u32),
+}
+
+fn main() {
+    println!("Hello, world!");
+    let number = 12;
+    match number {
+        1 => println!("one!"),
+        2 | 3| 5 |7 | 11 => println!("this is a prime"),
+        12..=19 => println!("a teen"),
+        _ => println!("ain't special"),
+    }
+    //解构元组
+    let pair = (0,-2);
+    match pair {
+        (0,y) => println!("first is 0 and y is {}",y),
+        (x,0) => println!("last is 0 and x is {}",x),
+        _ => println!("do not match any value "),
+    }
+    //解构enum
+    let color = Color::RGB(122, 17, 40);
+    // 试一试 ^ 将不同的值赋给 `color`
+
+    println!("What color is it?");
+    // 可以使用 `match` 来解构 `enum`。
+    match color {
+        Color::Red   => println!("The color is Red!"),
+        Color::Blue  => println!("The color is Blue!"),
+        Color::Green => println!("The color is Green!"),
+        Color::RGB(r, g, b) =>
+            println!("Red: {}, green: {}, and blue: {}!", r, g, b),
+        Color::HSV(h, s, v) =>
+            println!("Hue: {}, saturation: {}, value: {}!", h, s, v),
+        Color::HSL(h, s, l) =>
+            println!("Hue: {}, saturation: {}, lightness: {}!", h, s, l),
+        Color::CMY(c, m, y) =>
+            println!("Cyan: {}, magenta: {}, yellow: {}!", c, m, y),
+        Color::CMYK(c, m, y, k) =>
+            println!("Cyan: {}, magenta: {}, yellow: {}, key (black): {}!",
+                c, m, y, k),
+        // 不需要其它分支，因为所有的情形都已覆盖
+    }   
+}
