d7de699c051b6e10b94367847ce814baaa1d5c1a
liusen <liusen@miliantech.com>
2021-05-06 17:51:25 +0800

    fix

---
+++ ch18errorwrap/src/main.rs
@@ -0,0 +1,21 @@
+use std::num::ParseIntError;
+//first返回的是option；返回的事some（）和none；parse返回的是Result，返回的是Ok（），Err
+fn double_first(vec: Vec<&str>) -> Option<Result<i32, ParseIntError>> {
+    vec.first().map(|first| {
+        first.parse::<i32>().map(|n| 2 * n)
+    })
+}
+
+fn main() {
+    let numbers = vec!["42", "93", "18"];
+    let empty = vec![];
+    let strings = vec!["tofu", "93", "18"];
+
+    println!("The first doubled is {:?}", double_first(numbers));
+
+    println!("The first doubled is {:?}", double_first(empty));
+    // Error 1: the input vector is empty
+
+    println!("The first doubled is {:?}", double_first(strings));
+    // Error 2: the element doesn't parse to a number
+}
